a9c09ff087abb456a02eaa6fa7583a44440c3106
EC2 Default User <ec2-user@ip-172-31-64-64.ec2.internal>
2021-05-07 10:38:53 +0000

    add fizz_buzz

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,7 +1,183 @@
 # puts "hello,world"
 
-n = 1
-puts n = n + 1
-puts n += 1
-puts n -= 1
+# n = 1
+# puts n = n + 1
+# puts n += 1
+# puts n -= 1
 
+# ------------------
+
+# score = 59
+
+# if score >= 90
+#   puts "A"
+# elsif score >= 80
+#   puts "B"
+# elsif score >= 70
+#   puts "C"
+# else
+#   puts "D"
+# end
+
+# --------------------
+
+# animal = "cat"
+
+# if animal == "cat"
+#   puts "meow"
+# elsif animal == "dog"
+#   puts "bowbow"
+# elsif animal == "cow"
+#   puts "moomoo"
+# else
+#   puts "Not.fount"
+# end
+
+# ----------------------
+
+# age = 3
+
+# if age >= 12
+#   puts "5000円です"
+# elsif age >= 6
+#   puts "2500円です"
+# else
+#   puts "1000円です"
+# end
+
+# -----------------------
+
+# n = 0
+
+# unless n.zero?
+#   puts "Not.zero"
+# else
+#   puts "This is zero"
+# end
+
+# -------------------------
+
+# stone = "sapphire"
+
+# case stone
+# when "ruby"
+#   puts "july"
+# when "peridot"
+#   puts "August"
+# when "sapphire"
+#   puts "september"
+# else
+#   puts "Not.found"
+# end
+
+
+# -------------------------
+
+
+# def hello_world
+#   puts "hello,world"
+# end
+
+# hello_world
+
+# def add(x,y)
+#   x + y
+# end
+
+# puts add(10,1)
+
+# ---------------------------
+
+
+# def cry(animal)
+#   if animal == "cat"
+#     "meow"
+#   elsif animal == "bog"
+#     "bowbow"
+#   else
+#     "???"
+#   end
+# end
+
+# puts cry("cat")
+
+# -----------------------------
+
+
+# def fizz_buzz(n)
+  
+#   if n % 15 == 0
+#     "FizzBuzz"
+#   elsif n % 3 == 0
+#     "Fizz"
+#   elsif n % 5 == 0
+#     "Buzz"
+#   else
+#   n.to_s
+#   end
+  
+# end
+
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(7)
+# puts fizz_buzz(8)
+# puts fizz_buzz(9)
+# puts fizz_buzz(10)
+# puts fizz_buzz(11)
+# puts fizz_buzz(12)
+# puts fizz_buzz(13)
+# puts fizz_buzz(14)
+# puts fizz_buzz(15)
+
+
+# numbers = [1,2,3,4,5]
+
+# numbers.each do |number|
+#   puts number
+# end
+
+
+# def fizz_buzz(n)
+#   if n % 15 == 0
+#     "FizzBuzz"
+#   elsif n % 3 == 0
+#     "Fizz"
+#   elsif n % 5 == 0
+#     "Buzz"
+#   else
+#     n.to_s
+#   end
+# end
+
+# puts fizz_buzz(1)
+
+
+
+
+# -------------------------------
+
+
+# numbers = [1,2,3,4,5]
+
+# numbers.each do |number|
+#   puts number
+# end
+
+
+# colors = ["red", "green", "blue"]
+
+# colors.each do |color|
+#   puts color
+# end
+
+
+# numbers = [1,2,3,4,5]
+
+# for number in numbers do
+#   puts number
+# end
